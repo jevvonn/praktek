801e128e8453a4a645062c81825667c641a22549
jevvonn <darmawanb985@gmail.com>
2022-11-21 10:17:56 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return view("hello");
+})->name("hello");
+
+Route::get('/world', function () {
+    return view("world");
+})->name("world");
